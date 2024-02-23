3dc8c991fb52241f9f2c79aa6cb21fef1fb0718a
Scrpjon <160386739+Scrpjon@users.noreply.github.com>
2024-02-23 06:04:25 +0300

    Add files via upload

---
+++ java/123.js
@@ -0,0 +1,22 @@
+const select = document.querySelector('select');
+
+select.addEventListener('change',changeURLLanguage);
+
+function changeURLLanguage() {
+    let lang = select.value;
+    location.href = window.location.pathname + '#' + lang;
+    location.reload();
+}
+
+function changeLanguage() {
+    let hash = window.location.hash;
+    hash = hash.substr(1);
+    console.log(hash);
+    if (!allLang.includes(hash)) {
+        location.href = window.location.pathname + 'en';
+        location.reload();
+    }
+    select.value = hash
+}
+
+changeLanguage();
---
+++ java/javaT-L.js
@@ -0,0 +1,18 @@
+const langArr = {
+    "lng-main" : {
+        "ru": "главная",
+        "en": "main",
+    },
+    "lng-projects" : {
+        "ru": "проекты",
+        "en": "projects",
+    },
+    "lng-sets" : {
+        "ru": "наборы",
+        "en": "sets"
+    },
+    "lng-settings" : {
+        "ru": "настройки",
+        "en": "setting"
+    },
+}
